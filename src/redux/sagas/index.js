d9804b7f8bc812c8b4298db9993b1ac678e24d2f
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-14 17:21:39 -0600

    imported teesSaga and added to rootSaga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -4,6 +4,7 @@ import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
 import detailsSaga from './detailsSaga';
 import coursesSaga from './coursesSaga';
+import teesSaga from './teesSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -19,5 +20,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     coursesSaga(),
+    teesSaga(),
   ]);
 }
