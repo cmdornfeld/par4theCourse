4ea3fbbb9f019b37f1a9b95c310bd0c30c8cf9fa
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-13 20:34:09 -0600

    added queryText and updated query string to DB

--- server/routes/details.router.js
+++ server/routes/details.router.js
@@ -8,7 +8,14 @@ const {rejectUnauthenticated} = require('../modules/authentication-middleware')
  */
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query(`SELECT * FROM "round" WHERE "user_id" = $1;`, [req.user.id])
+    let queryText = `SELECT SUM("hole_user"."score") score, "course"."name", "hole_user"."round_id", "users"."username"
+                    FROM "round"
+                    JOIN "course" ON "course"."id" = "round"."course_id"
+                    JOIN "hole_user" ON "hole_user"."round_id" = "round"."id"
+                    JOIN "users" ON "users"."id" = "round"."user_id"
+                    WHERE "users"."id" = $1
+                    GROUP BY "course"."name", "hole_user"."round_id", "users"."username";`
+    pool.query(queryText, [req.user.id])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error GETTING details:', error);
