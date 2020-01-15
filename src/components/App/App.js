869774095343278747134a80ae628908c47418a6
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-15 09:11:15 -0600

    imported Holes component to App.js and added as Protected Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,17 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
-
 import {connect} from 'react-redux';
-
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
-
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
-
 import './App.css';
 import NewRound from '../NewRound/NewRound';
+import Holes from '../Holes/Holes';
 
 class App extends Component {
   componentDidMount () {
@@ -61,6 +58,12 @@ class App extends Component {
               exact
               path ="/new-round"
               component={NewRound}
+            />
+
+            <ProtectedRoute
+              exact
+              path ="/new-round/hole-1"
+              component={Holes}
             />
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
