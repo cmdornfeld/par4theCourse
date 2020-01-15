4282946850bc3dc410f708223a0c37fead8aac37
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-15 15:43:58 -0600

    updated queryText and added console log

--- server/routes/tees.router.js
+++ server/routes/tees.router.js
@@ -4,11 +4,14 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
+    console.log(req.query);
+    
     let queryText = `SELECT "teeColor"."name", "distance"
                     FROM "tees"
                     JOIN "teeColor" ON "teeColor"."id" = "tees"."color"
-                    JOIN "course" ON "course"."id" = "tees"."course_id";`
-    pool.query(queryText)
+                    JOIN "course" ON "course"."id" = "tees"."course_id"
+                    WHERE "course"."name" = $1`
+    pool.query(queryText, [req.query.course])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error GETTING tees:', error);
