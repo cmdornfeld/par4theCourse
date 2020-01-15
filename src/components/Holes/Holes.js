d0b906f394ffecb606e27ad31256f32d6d5d02ef
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-15 10:07:30 -0600

    added simple layout to Holes.js

--- src/components/Holes/Holes.js
+++ src/components/Holes/Holes.js
@@ -8,9 +8,17 @@ class Hole1 extends Component {
 
   render() {
     return (
-      <div>
-        {JSON.stringify(this.props.holes)}
-      </div>
+        <div>
+            <h1>Hole {/*{this.props.holes} */}</h1>
+            {JSON.stringify(this.props.holes)}
+            <label>My score</label>
+            <input type="number" min="1" max="10" placeholder="enter your score" /><br/>
+            <label>Par:</label><span>{/*{this.props.holes}*/}</span><br/>
+            <label>Comments:</label>
+            <textarea></textarea><br/>
+            <button>Cancel Round</button>
+            <button>Submit Score</button>
+        </div>
     );
   }
 }
