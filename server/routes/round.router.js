60829a2a857c3ea55f8c140b51cfd9084de3b616
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-16 22:53:04 -0600

    updated round.router.js POST route

--- server/routes/round.router.js
+++ server/routes/round.router.js
@@ -2,14 +2,15 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.post('/round', (req, res, next) => {
+router.post('/', (req, res) => {
     console.log('logging req.body', req.body);
     
-  
-    const queryText = 'INSERT INTO "round" ("user_id", "course_id", "tee_id") VALUES ($1, $2, $3) RETURNING id;';
-    pool.query(queryText, [$1, $2, $3])
-      .then(() => res.sendStatus(201))
-      .catch(() => res.sendStatus(500));
+    const queryText = 'INSERT INTO "round" ("holes", "user_id", "course_id", "tee_id") VALUES ($1, $2, $3, $4) RETURNING id;';
+    pool.query(queryText, [req.body.holes, req.body.user, req.body.course, req.body.tee])
+      .then((result) => {console.log('logging', result)
+      res.send(result).status(201)})
+      .catch((error) => {console.log('logging', error);
+       res.sendStatus(500)});
   });
 
 
