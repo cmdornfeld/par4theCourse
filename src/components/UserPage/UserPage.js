23ad6001d56bdcf5a0a74edd3a5d058dbd93069b
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-17 11:59:09 -0600

    changed infopage to be userpage, and deleted infopage

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
-// this could also be written with destructuring parameters as:
-// const UserPage = ({ user }) => (
-// and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
-  </div>
-);
+const styles = {
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
-// Instead of taking everything from state, we just want the user info.
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
-});
+class InfoPage extends Component {
+  componentDidMount() {
+    this.props.dispatch({type: 'GET_DETAILS'});
+  }
 
-// this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(UserPage);
+  goToSetRoundDetails = () => {
+    this.props.history.push('/new-round')
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello, <b>{this.props.user.username}</b></p><br/>
+          <Button onClick={this.goToSetRoundDetails} className={this.props.classes.button} variant="contained">
+            Start New Round
+          </Button><br/>
+          {JSON.stringify(this.props.user)}<br/>
+          {JSON.stringify(this.props.details)}
+          <div className="card">
+          {this.props.details.map(round => (
+            <Card key={round.round_id} className={this.props.classes.card}>
+              <CardContent>
+                {round.score}<br/>{round.name}<br/> Round ID: {round.round_id}
+              </CardContent>
+            </Card>
+          ))}
+          </div>
+      </div>
+    );
+  }
+}
+
+const putReduxStateOnProps = (reduxStore) => ({
+  details: reduxStore.details,
+  user: reduxStore.user
+})
+
+export default connect(putReduxStateOnProps)(withStyles(styles)(InfoPage));
