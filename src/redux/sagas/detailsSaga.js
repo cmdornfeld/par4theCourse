e1799c6fd581b87a5771f73c38e5dd0fc764687e
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-19 12:13:24 -0600

    added 2nd generator function to getDetails

--- src/redux/sagas/detailsSaga.js
+++ src/redux/sagas/detailsSaga.js
@@ -11,8 +11,19 @@ function* getDetailsSaga() {
     }
 }
 
+function* getRoundDetailsSaga(action) {
+    try{
+        const getResponse = yield axios.get(`/api/details/${action.payload}`);
+        yield put({type: 'SET_ROUND_DETAILS', payload: getResponse.data})
+    }
+    catch (error){
+        console.log(error); 
+    }
+}
+
 function* getDetails() {
-    yield takeLatest('GET_DETAILS', getDetailsSaga)
+    yield takeLatest('GET_DETAILS', getDetailsSaga);
+    yield takeLatest('GET_ROUND_DETAILS', getRoundDetailsSaga);
 }
 
 export default getDetails;
