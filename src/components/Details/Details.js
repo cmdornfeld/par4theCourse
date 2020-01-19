082286dc9bd5f0b25c9b087f836cec7591aeb235
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-19 12:07:28 -0600

    updated Details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,17 +3,31 @@ import { connect } from 'react-redux';
 
 
 class Details extends Component {
-  componentDidMount() {
-    this.props.dispatch({type: 'GET_ROUND_DETAILS'});
-  }
+  
 
   render() {
     return (
+      <>
       <div>
         <h1>Hello, <b>{this.props.user.username}</b></h1><br/>
           {JSON.stringify(this.props.user)}<br/>
           {JSON.stringify(this.props.details)}
       </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Hole #</th><th>Par</th><th>Score</th><th>Comments</th><th>&nbsp;</th><th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {this.props.details.map(hole => {
+            <tr>
+              <td>{hole.number}</td><td>{hole.par}</td><td>{hole.score}</td><td>{hole.comments}</td>
+            </tr>
+          })} */}
+        </tbody>
+      </table>
+      </>
     );
   }
 }
