80d70c144e71e1ba8b0d6e7c0f0acb6e67f7ff90
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-19 18:02:22 -0600

    updated pool query requests in courses.router.js

--- server/routes/courses.router.js
+++ server/routes/courses.router.js
@@ -4,21 +4,21 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    pool.query(`SELECT "name", "holes", "id" FROM "course";`)
+    pool.query(`SELECT "name", "holes", "location", "id" FROM "course";`)
         .then(results => res.send(results.rows))
         .catch(error => {
-            console.log('Error GETTING details:', error);
+            console.log('Error GETTING course details:', error);
             res.sendStatus(500);
         });
 });
 
 router.get('/holes', (req, res) => {
-    pool.query(`SELECT "number", "par" FROM "hole_course"
+    pool.query(`SELECT "number", "par", "hole_course"."id" FROM "hole_course"
                 JOIN "course" ON "course"."id" = "hole_course"."course_id"
                 WHERE "course"."id" = $1;`, [req.query.courseId])
         .then(results => res.send(results.rows))
         .catch(error => {
-            console.log('Error GETTING details:', error);
+            console.log('Error GETTING holes details:', error);
             res.sendStatus(500);
         });
 });
