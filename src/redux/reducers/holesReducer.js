7a36ab9f646d519d271ae65004a3f9e78e1a1225
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-21 14:40:04 -0600

    commented out holesReducer; deleting

--- src/redux/reducers/holesReducer.js
+++ src/redux/reducers/holesReducer.js
@@ -1,14 +1,14 @@
-const getHoles = (state={holeIndex: 0, holeData: []}, action) => {
-    switch(action.type) {
-        case 'SET_HOLES': 
-            return {
-                holeIndex: 0,
-                holeData: action.payload
-            }
-        case 'SET_HOLE_INDEX': 
-            return {...state, holeIndex: action.payload}
-        default: return state
-    }
-}
+// const getHoles = (state={holeIndex: 0, holeData: []}, action) => {
+//     switch(action.type) {
+//         case 'SET_HOLES': 
+//             return {
+//                 holeIndex: 0,
+//                 holeData: action.payload
+//             }
+//         case 'SET_HOLE_INDEX': 
+//             return {...state, holeIndex: action.payload}
+//         default: return state
+//     }
+// }
 
-export default getHoles;
+// export default getHoles;
