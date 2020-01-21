9354069527ed752504fb989e03992faeeb63288b
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-21 14:40:25 -0600

    commented out holesSaga; deleting

--- src/redux/sagas/holesSaga.js
+++ src/redux/sagas/holesSaga.js
@@ -1,28 +1,28 @@
-import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
+// import axios from 'axios';
+// import { put, takeLatest } from 'redux-saga/effects';
 
-function* getHolesSaga(action) {
-    try{
-        const getResponse = yield axios.get(`/api/courses/holes?courseId=${action.payload}`);
-        yield put({type: 'SET_HOLES', payload: getResponse.data})
-    }
-    catch (error){
-        console.log(error); 
-    }
-}
+// function* getHolesSaga(action) {
+//     try{
+//         const getResponse = yield axios.get(`/api/courses/holes?courseId=${action.payload}`);
+//         yield put({type: 'SET_HOLES', payload: getResponse.data})
+//     }
+//     catch (error){
+//         console.log(error); 
+//     }
+// }
 
-function* postHolesSaga(action) {
-    try{
-        yield axios.post('/api/details', action.payload);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+// function* postHolesSaga(action) {
+//     try{
+//         yield axios.post('/api/details', action.payload);
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+// }
 
-function* getHoles() {
-    yield takeLatest('GET_COURSE_HOLES', getHolesSaga);
-    yield takeLatest('POST_HOLE_INFO', postHolesSaga);
-}
+// function* getHoles() {
+//     yield takeLatest('GET_COURSE_HOLES', getHolesSaga);
+//     yield takeLatest('POST_HOLE_INFO', postHolesSaga);
+// }
 
-export default getHoles;
+// export default getHoles;
